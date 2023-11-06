146531fa426007a47f40bb90bcde91ac625e5ca4
Shihab Hossain <64682561+Shihabagent47@users.noreply.github.com>
2023-11-06 23:39:33 +0600

    worked on conneciting front end

--- public/bus_reservation_frontend/script.js
+++ public/bus_reservation_frontend/script.js
@@ -8,36 +8,38 @@
 
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    const submitButton = document.getElementById('submit');
-    const usernameInput = document.getElementById('username');
-    const passwordInput = document.getElementById('password');
-    const errorMessage = document.getElementById('error_message');
+function setupLogin(endpoint) {
+    document.addEventListener('DOMContentLoaded', function () {
+        const submitButton = document.getElementById('submit');
+        const usernameInput = document.getElementById('username');
+        const passwordInput = document.getElementById('password');
+        const errorMessage = document.getElementById('error_message');
 
-    submitButton.addEventListener('click', async function () {
-        const username = usernameInput.value;
-        const password = passwordInput.value;
+        submitButton.addEventListener('click', async function () {
+            const username = usernameInput.value;
+            const password = passwordInput.value;
 
-        try {
-            const response = await fetch('/loginAdmin', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ username, password }),
-            });
+            try {
+                const response = await fetch(endpoint, { // Use the provided endpoint
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ username, password }),
+                });
 
-            if (response.ok) {
-                // Login successful
-                window.location.href = '/bus_reservation_frontend/home.html'; // Redirect to another page
-            } else {
-                // Handle login error
-                const data = await response.json();
-                errorMessage.textContent = data.message;
+                if (response.ok) {
+                    // Login successful
+                    window.location.href = '/bus_reservation_frontend/home.html'; // Redirect to another page
+                } else {
+                    // Handle login error
+                    const data = await response.json();
+                    errorMessage.textContent = data.message;
+                }
+            } catch (error) {
+                console.error('Login error:', error);
+                errorMessage.textContent = 'An error occurred during login.';
             }
-        } catch (error) {
-            console.error('Login error:', error);
-            errorMessage.textContent = 'An error occurred during login.';
-        }
+        });
     });
-});
+}
